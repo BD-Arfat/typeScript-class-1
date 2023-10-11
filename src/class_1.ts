36750d4c6113cd56b28206a66f693578af1e0588
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-11 22:25:03 +0600

    add

---
+++ src/class_1.ts
@@ -0,0 +1,34 @@
+// TypeScript Simple Types
+
+
+// There are three main primitives in JavaScript and TypeScript.
+
+/**
+ * 1) string
+ * 2) boolean
+ * 3) number
+ * */
+
+// When creating a variable, there are two main ways TypeScript assigns a type:
+
+/**
+ * 1) Explicit Type
+ * 2) Implicit Type
+ * 
+*/
+
+// some Explicit Type
+
+let man: string = 'arfat';
+let number: number = 949494;
+let isAdmin: boolean = true;
+
+let taka: string = 'amar theke ekhon 5000 taka ache, niba !!!!';
+let takaNumber: number = 852369;
+let takaLagbe: boolean = true;
+
+
+// 
+
+let firesName: string = 'hi mr shofi';
+firesName = 'arfat';
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-// some Explicit Type
-
-const mars : string = 'arfat'
-const number : number = 552;
-const isAdmin : boolean = true;
