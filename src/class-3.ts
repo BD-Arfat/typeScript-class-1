d21150cc628f95c0d08934e312d5eb5bfbb37e80
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-11 23:24:18 +0600

    add

---
+++ src/class-3.ts
@@ -0,0 +1,20 @@
+// TypeScript Arrays
+
+
+let nam: string[] = ['arfat', 'rifat', 'lisa', 'tisha'];
+let numberss: number[] = [21, 25, 84, 51, 32,];
+
+numberss.push(854);
+
+
+
+// TypeScript Tuples
+
+let ourTuples: ['arfat', 'rifat', 521, 321, false];
+
+let ourTuple: [number, string, boolean]
+
+// Tuples
+let user: [number, string] = [444, 'taka'];
+
+let boyfriends: [string, string] = ['lolo', 'lolita']
