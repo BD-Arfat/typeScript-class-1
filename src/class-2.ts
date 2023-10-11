786bb9baf4223110331b8e31781a196648302a05
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-11 22:37:31 +0600

    add

---
+++ src/class-2.ts
@@ -0,0 +1,17 @@
+// TypeScript Special Types
+
+
+// Type: any
+
+// Example without any
+let n = true;
+// n = 'arfat ih kemon acho'
+
+let v: any = true;
+v = 'arfat hi kemon acho';
+
+
+let w: unknown = 7;
+w = 'string';
+
+
