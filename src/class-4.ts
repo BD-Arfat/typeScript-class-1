260f69dab25a0cf3bfe150765d8571d690857620
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-12 10:12:57 +0600

    add

---
+++ src/class-4.ts
@@ -0,0 +1,19 @@
+// TypeScript Object Types
+
+let users: {
+    name: string,
+    email: string,
+    phone: string,
+    age:number,
+    gf:boolean
+} = {
+    name : 'arfat',
+    email : 'arfat@gmail.com',
+    phone : '01846615162',
+    age : 18,
+    gf : false
+}
+
+users.phone = 'phone number!!!'
+// users.phone = 9999;   (error)
+
